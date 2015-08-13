0e8c5fa130c8286055b6d60f9d2921db68e5f7ff
Harpreet S <hsingh@wickr.com>
2015-08-13 17:36:57 -0400

    few more

--- app/src/main/java/com/harry/MainActivity.java
+++ app/src/main/java/com/harry/MainActivity.java
@@ -14,8 +14,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        Log.d("test", String.valueOf(Utility.isAnagram1("word", "wrdo")));
-        Log.d("test", String.valueOf(Utility.firstNonRepeatingCharacter("daxxyananda")));
-
+//        Log.d("test", String.valueOf(Utility.firstNonRepeatingCharacter("daxxyananda")));
+//        Log.d("test", String.valueOf(Utility.reverseString("hi how are you?")));
+//        Log.d("test", String.valueOf(Utility.stringContainsOnlyNumbers("884832j4823")));
+//        Log.d("test", String.valueOf(Utility.otherCharacters("hello 234 so325$@@@#%^")));
+        Log.d("test", String.valueOf(Utility.allDuplicateCharactersInString("error")));
     }
 
 ///
--- app/src/main/java/com/harry/Utility.java
+++ app/src/main/java/com/harry/Utility.java
@@ -1,12 +1,52 @@
 package com.harry;
 
+import java.util.ArrayList;
+
 /**
  * Created by hsingh on 8/13/15.
  */
 public class Utility {
 
+    public static ArrayList<Character> allDuplicateCharactersInString(String string) {
+        ArrayList<Character> words = new ArrayList<Character>();
+        for (int i = 0; i < string.length(); i++) {
+            boolean found = false;
+            for (int j = 0; j < string.length(); j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (string.charAt(i) == string.charAt(j)) {
+                    found = true;
+                    break;
+                }
+            }
+            if(found) {
+                words.add(string.charAt(i));
+            }
+        }
+        return words;
+    }
+    public static char firstDuplicateCharactersInString(String string) {
+        int t=0;
+        for (int i = 0; i < string.length(); i++) {
+            boolean found = false;
+            for (int j = 0; j < string.length(); j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (string.charAt(i) == string.charAt(j)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) {
+                t=i;
+            }
+        }
+        return string.charAt(t);
+    }
     public static char firstNonRepeatingCharacter(String string) {
-        char[] chars = string.toCharArray();
+//        char[] chars = string.toCharArray();
         int t=0;
         for (int i = 0; i < string.length(); i++) {
             boolean found = true; // first one is by default different and non-repeating before logic
@@ -14,7 +54,7 @@ public class Utility {
                 if (i == j) { // no need to check iteration which is matched
                     continue;
                 }
-                if (chars[i] == chars[j]) {
+                if (string.charAt(i) == string.charAt(j)) {
                     found = false; // if some char is equal then make it false as it is existing
                     break;
                 }
@@ -23,8 +63,49 @@ public class Utility {
                 t=i;
             }
         }
-        return chars[t];
+        return string.charAt(t);
     }
+
+    public static ArrayList<Character> otherCharacters(String string) {
+        ArrayList<Character> words = new ArrayList<Character>();
+        for (int i = 0; i < string.length(); i++) {
+            if(string.charAt(i)==' '){
+                continue;
+            }
+            if (!Character.isAlphabetic(string.charAt(i))) {
+                words.add(string.charAt(i));
+            }
+        }
+        return words;
+    }
+
+    public static boolean stringContainsOnlyNumbers(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            if (!Character.isDigit(string.charAt(i))) {
+                return false;
+            }
+        }return true;
+    }
+
+    public static String reverseString(String string) {
+        char[] words = string.toCharArray();
+        char[] newWords = new char[string.length()];
+        for(int i=0;i<words.length;i++) {
+            newWords[i] = words[string.length() - 1-i];
+        }
+        return new String(newWords);
+    }
+
+    public static String reverseString1(String string) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=string.length()-1;i>=0;i--) {
+            sb.append(string.charAt(i));
+        }
+        return sb.toString();
+//        return sb.reverse().toString();
+    }
+
+
 
     public static boolean isAnagram(String first, String second) {
         if (first.length() != second.length()) {
